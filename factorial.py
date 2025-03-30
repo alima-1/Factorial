7dcd3abb6e995d92e3cd9c0aae35f5bd86f2ee01
ayebaishmo <ishmo255super@gmail.com>
2025-03-30 21:09:50 +0300

    Added factorial code for 0 and 1 and test function

--- factorial.py
+++ factorial.py
@@ -1,5 +1,8 @@
 def factorial(value):
     if value < 0:
          raise ValueError("Factorial is not defined for negative numbers.")
+    
+    elif value == 0 or value == 1:
+         return 1
 
 
--- test_factorial.py
+++ test_factorial.py
@@ -1,6 +1,11 @@
 import pytest
 from factorial import factorial
 
+
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+
 def test_factorial_neg():
     with pytest.raises(ValueError):
         factorial(-5)
