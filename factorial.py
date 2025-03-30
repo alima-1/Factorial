cffe9256e261c4847a1da35c90f4e9fa178ec057
alima <alima.ayikoru1@gmail.com>
2025-03-30 21:32:17 +0300

    refactored using Black

--- factorial.py
+++ factorial.py
@@ -1,14 +1,12 @@
 def factorial(value):
     if value < 0:
-         raise ValueError("Factorial is not defined for negative numbers.")
-    
+        raise ValueError("Factorial is not defined for negative numbers.")
+
     elif value == 0 or value == 1:
-         return 1
-    
+        return 1
+
     elif value is str:
-         raise TypeError("Factorial is not defined for strings")
-    
+        raise TypeError("Factorial is not defined for strings")
+
     else:
-         return value*factorial(value - 1)
-
-
+        return value * factorial(value - 1)
--- test_factorial.py
+++ test_factorial.py
@@ -8,9 +8,11 @@ def test_factorial():
     assert factorial(4) == 24
     assert factorial(5) == 120
 
+
 def test_factorial_neg():
     with pytest.raises(ValueError):
         factorial(-5)
+
 
 def test_factorial_str():
     with pytest.raises(TypeError):
