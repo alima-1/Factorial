9a330f2bbc6fdd8c000225968500e488f2f133ee
alima <alima.ayikoru1@gmail.com>
2025-03-30 21:20:14 +0300

    Wrote code to cater for the other values and tested it.

--- factorial.py
+++ factorial.py
@@ -4,5 +4,8 @@ def factorial(value):
     
     elif value == 0 or value == 1:
          return 1
+    
+    else:
+         return value*factorial(value - 1)
 
 
--- test_factorial.py
+++ test_factorial.py
@@ -5,6 +5,8 @@ from factorial import factorial
 def test_factorial():
     assert factorial(0) == 1
     assert factorial(1) == 1
+    assert factorial(4) == 24
+    assert factorial(5) == 120
 
 def test_factorial_neg():
     with pytest.raises(ValueError):
